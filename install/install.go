f37690ed09a3e43d34e082caa0257908c8aa3b79
Bruno Piaui <brunopiaui@gmail.com>
2019-07-22 11:03:09 -0300

    Including importJSONTasks on install

--- install/install.go
+++ install/install.go
@@ -1,12 +1,13 @@
 package install
 
 import (
+	"github.com/agile-work/srv-mdl-shared/models/job"
 	"github.com/agile-work/srv-mdl-shared/models/module"
 	"github.com/agile-work/srv-shared/constants"
 	"github.com/agile-work/srv-shared/sql-builder/db"
 )
 
-// Init initialize the module instalation process
+// Init initialize the module installation process
 func Init() error {
 	mdl := &module.Module{
 		ID:     db.UUID(),
@@ -19,10 +20,17 @@ func Init() error {
 		return err
 	}
 
+	jobInstance := &job.Instance{}
+	if err := jobInstance.CreateFromJSON(trs, "admin", "./install/test.json", 60, make(map[string]interface{})); err != nil {
+		trs.Rollback()
+		return err
+	}
+
 	if err := mdl.Register(trs); err != nil {
 		trs.Rollback()
 		return err
 	}
 
+	trs.Commit()
 	return nil
 }
